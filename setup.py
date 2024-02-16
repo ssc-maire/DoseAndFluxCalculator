bcfc6446c0d71a992c674a52a62bb974acdbce40
Chris Davis <ChrisSWDavis@gmail.com>
2024-02-16 12:33:21 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,11 @@ if os.path.isfile(requirement_path):
     with open(requirement_path) as f:
         install_requires = f.read().splitlines()
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='atmosphericRadiationDoseAndFlux',
     packages=find_packages(exclude='tests'),
@@ -16,8 +21,13 @@ setup(
     include_package_data=True,
     version='1.0.0',
     description='Python library for calculating doses and fluxes at a particular altitude given an input spectrum',
-    author='Me',
-    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Chris S. W. Davis',
+    author_email='ChrisSWDavis@gmail.com',
+    url='https://github.com/ssc-maire/DoseAndFluxCalculator',
+    keywords = 'atmosphere radiation space weather cosmic rays spectrum alpha particles yield response dose rates ambient effective GLE sun solar aviation neutron monitor',
+    license='GNU General Public License v3.0',
     install_requires=install_requires,
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
